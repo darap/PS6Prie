08f2ca3989877dafeae22ab9e9e29f28ad61c646
Dara <Dara@128.4.105.181>
2017-05-04 23:28:20 -0400

    Final changes to PS6.

--- PS6_DAL/src/test/java/base/Person_Test.java
+++ PS6_DAL/src/test/java/base/Person_Test.java
@@ -2,7 +2,9 @@ package base;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.UUID;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -13,36 +15,69 @@ import org.junit.Test;
 import domain.PersonDomainModel;
 
 public class Person_Test {
-
+	
+	
+	private static PersonDomainModel p1 = new PersonDomainModel();
+	
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+	
+	
+	
+	p1.setFirstName("Dara");
+	p1.setLastName("Priester");
+	p1.setBirthday(new Date(0));
+	p1.setCity("Newark");
+	p1.setStreet("121 Mary Rd");
+	p1.setPostalCode(19705);
+	
+	PersonDAL.addPerson(p1);
+	PersonDomainModel p2 = PersonDAL.getPerson(p1.getPersonID());
+	assertNotNull(p2);
+	
+	
+	
 	}
-
+	
+	
+	
+	
+	
+	
+	@Test
+	public void testUpdateDelete(){
+	PersonDomainModel p2 = PersonDAL.getPerson(p1.getPersonID());
+	assertEquals(p1.getPersonID(), p2.getPersonID());
+	p2.setLastName("Johnson");
+	PersonDAL.updatePerson(p2);
+	
+	PersonDomainModel p3 = PersonDAL.getPerson(p1.getPersonID());
+	assertEquals(p2.getLastName(), p3.getLastName()); 
+	assertNotEquals(p1.getLastName(),p3.getLastName());
+	
+	
+	PersonDAL.deletePerson(p1.getPersonID());
+	
+	PersonDomainModel p4 = PersonDAL.getPerson(p1.getPersonID());
+	
+	assertNull(p4);
+	
+	
+	
+	}
+	@Test 
+	public void testGetAll(){
+	ArrayList<PersonDomainModel> pers = PersonDAL.getPersons();
+	assertNotNull(pers);
+	}
+	
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+	PersonDAL.deletePerson(p1.getPersonID());
+	
 	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void AddPerson() {
-		PersonDomainModel per1 = new PersonDomainModel();
-		per1.setBirthday(new Date(0));
-		per1.setCity("Townsend");
-		per1.setFirstName("Bert");
-		per1.setLastName("Gibbons");
-		per1.setPostalCode(19734);
-		per1.setStreet("214 Labrador Lane");
-		
-		PersonDAL.addPerson(per1);
-		
-		
-	}
-
+	
+	
 }
+
